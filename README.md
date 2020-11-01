abf988c091cb3107f215eb0548890aac8c791ee7
Gourish Sadhu <sadhu5gourish@outlook.com>
2020-11-01 10:40:53 +0530

    handled routes / and /forecast

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fetcher-api
+### A simple backend for fetching data from api and giving response
--- src/main/java/com/fetcher/apidata/App.java
+++ src/main/java/com/fetcher/apidata/App.java
@@ -1,13 +1,28 @@
 package com.fetcher.apidata;
 
 /**
- * Hello world!
+ * A simple backend server for fetching data from api and giving response
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class App {
+
+	private final static Integer PORT = Integer.valueOf(System.getenv("PORT"));
+
+    public static void main( String[] args ) throws IOException {
+    	HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+
+    	server.createContext("/", new BaseHandler());
+    	server.createContext("/forecast", new ForecastHandler());
+    	// create Route Weather with handler WeatherHandler()
+
+    	server.setExecutor(null);
+    	server.start();
+    	System.out.println("[ Server ] listening at http://localhost:" + PORT);
     }
 }
