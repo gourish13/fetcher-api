b75574484512621e791fde4c4681c77fe39cadf7
Gourish Sadhu <sadhu5gourish@outlook.com>
2020-11-01 21:44:57 +0530

    .gitignore updated

--- src/main/java/com/fetcher/apidata/RoutesHandler.java
+++ src/main/java/com/fetcher/apidata/RoutesHandler.java
@@ -14,7 +14,7 @@ import com.sun.net.httpserver.HttpHandler;
 class BaseHandler implements HttpHandler {
 	@Override
 	public void handle(HttpExchange httpExc) throws IOException {
-		String res = "{\"cod\": \"404\", \"sample_req_types\": [\"/weather?city=cityName\", \"/weather?city=city_name,country_code\", \"/forecast?city=cityName\", \"/forecast?city=city_name,country_code\"]}";
+		String res = "{\"cod\": \"404\", \"allowed_req_types\": [\"/weather?city=cityName\", \"/weather?city=city_name,country_code\", \"/forecast?city=cityName\", \"/forecast?city=city_name,country_code\"]}";
 		httpExc.getResponseHeaders().set("Content-Type", "application/json");
 		httpExc.sendResponseHeaders(200, res.length());
 		OutputStream ot = httpExc.getResponseBody();
@@ -26,13 +26,13 @@ class BaseHandler implements HttpHandler {
 class ForecastHandler implements HttpHandler {
 	@Override
 	public void handle(HttpExchange httpExc) throws IOException {
-		httpExc.getResponseHeaders().set("Content-Type", "application/json");
 
 		String[] cityQuery = httpExc.getRequestURI().toString().split("\\?")[1].split("=");
 		
 		if (cityQuery.length != 2)
 			httpExc.sendResponseHeaders(404, -1);
 		else {
+			httpExc.getResponseHeaders().set("Content-Type", "application/json");
 			String jsonRes = new FetchJSON().getJSONResponse("forecast", cityQuery[1]);
 			httpExc.sendResponseHeaders(200, jsonRes.length());
 			OutputStream ot = httpExc.getResponseBody();
