adf6db75ac659fed78ef97206855cd0a1f7c9428
gourish13 <62661395+gourish13@users.noreply.github.com>
2020-11-17 12:58:02 +0530

    Update App.java

--- src/main/java/com/fetcher/apidata/App.java
+++ src/main/java/com/fetcher/apidata/App.java
@@ -12,18 +12,18 @@ import com.sun.net.httpserver.HttpServer;
 
 public class App {
 
-	private final static Integer PORT = Integer.valueOf(System.getenv("PORT"));
+	private final static Integer PORT = Integer.valueOf(System.getenv("PORT"));  // gets the port specified from the environment variable PORT
 
     public static void main( String[] args ) throws IOException {
-    	HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
-
+    	HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);  // Creating a very basic server
+	    
+	// Creating Routes and assigning routes handler to them
     	server.createContext("/", new BaseHandler());
     	server.createContext("/forecast", new ForecastHandler());
     	server.createContext("/weather" , new WeatherHandler());
-    	// create Route Weather with handler WeatherHandler()
 
     	server.setExecutor(null);
-    	server.start();
+    	server.start();  // Starting the server
     	System.out.println("[ Server ] listening at Port : " + PORT);
     }
 }
